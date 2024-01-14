cb2e9b781fe3dd2a2acdd9ffcf3f36618f5aec31
Young999999999 <150986163+Young999999999@users.noreply.github.com>
2024-01-14 19:01:31 +0900

    [Gold IV] Title: 두 로봇, Time: 472 ms, Memory: 326856 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Gold/15971. 두 로봇/두 로봇.py
@@ -0,0 +1,39 @@
+import sys
+sys.setrecursionlimit(100000)
+input= sys.stdin.readline
+
+dist = []
+
+def dfs(start,target):
+    visited[start] = True
+
+    if start == target :
+        print(sum(dist) - max(dist))
+        return
+
+    for next,cost in graph[start]:
+        if not visited[next]:
+            dist.append(cost)
+            dfs(next,target)
+            dist.pop()
+
+
+N,S,E = map(int,input().split())
+
+graph=[[] for i in range(N+1)]
+
+for i in range(N-1):
+    s,e,cost =map(int,input().split())
+    graph[s].append((e,cost))
+    graph[e].append((s,cost))
+
+if S == E :
+    print(0)
+    exit(0)
+
+visited=[False] * (N+1)
+dfs(S,E)
+
+
+
+
