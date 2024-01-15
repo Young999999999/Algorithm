c93978a516af64ea7b6372e72f5883ef4fd9fe46
Young999999999 <150986163+Young999999999@users.noreply.github.com>
2024-01-15 20:57:16 +0900

    [Gold V] Title: 호숫가의 개미굴, Time: 200 ms, Memory: 157568 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/28325. 호숫가의 개미굴/호숫가의 개미굴.py
+++ 백준/Gold/28325. 호숫가의 개미굴/호숫가의 개미굴.py
@@ -15,13 +15,27 @@ n = int(input())
 c =list(map(int,input().split()))
 visited=[False]*(n)
 
-
 result = sum(c)
 
-
+locate =0
 for i in range(n):
-    if c[i] == 0 :
-        judge(i)
+    if c[i] > 0 :
+        locate = i
+        break
 
 
-print(result+visited.count(True))
+
+if result == 0: #쪽방이 없는 경우 어느 부분을 start로 잡든 상관이 없음
+    for i in range(n):
+        if c[i] == 0 :
+            judge(i)
+
+else: # 쪽방이 있는경우 쪽방 옆의 방을 무조건 선택해야함.
+    for i in range(locate+1,locate+1+n):
+        cur = i % n
+
+        if c[cur] == 0:
+            judge(cur)
+
+SUM = visited.count(True)
+print(result+SUM)
