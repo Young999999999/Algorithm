013dea124efcbf55ab0a3193a3218e7f90cd959e
Young999999999 <150986163+Young999999999@users.noreply.github.com>
2024-03-05 22:44:49 +0900

    [Silver I] Title: 계산 로봇, Time: 172 ms, Memory: 111824 KB, Score: 41 point -BaekjoonHub

--- 백준/Silver/22342. 계산 로봇/계산 로봇.py
+++ 백준/Silver/22342. 계산 로봇/계산 로봇.py
@@ -8,31 +8,27 @@ for i in range(n):
 robot = [[[0,0,0] for i in range(m)] for j in range(n)]
 
 
-# def setStore(x,y):
-#     global result
-#
-#     MAX = 0
-#     for dy in [y-1,y,y+1]:
-#         if -1<dy<n and -1<x-1:
-#             MAX = max(MAX,robot[dy][x-1][2])
-#
-#     result = max(MAX,result)
-#
-#     robot[y][x][1] = MAX
-#     robot[y][x][2] = robot[y][x][1] + matrix[y][x]
+def setStore(x,y):
+    global result
+
+    MAX = 0
+    for dy in [y-1,y,y+1]:
+        if -1<dy<n and -1<x-1:
+            MAX = max(MAX,robot[dy][x-1][2])
+
+    result = max(MAX,result)
+    robot[y][x][1] = MAX
+
+def setOutput(x,y):
+    robot[y][x][2] = robot[y][x][1] + matrix[y][x]
+
 
 result = 0
 
-for x in range(m):
-    for y in range(n):
-        MAX = 0
-        for dy in [y-1,y,y+1]:
-            if -1<dy<n and -1<x-1:
-                MAX = max(MAX,robot[dy][x-1][2])
-
-        result = max(MAX,result)
-        robot[y][x][1] = MAX
-        robot[y][x][2] = robot[y][x][1] + matrix[y][x]
+for i in range(m):
+    for j in range(n):
+        setStore(i,j)
+        setOutput(i,j)
 
 
 print(result)
