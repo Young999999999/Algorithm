b23a7ce905e3ca4d21c550e64ee815bbf1f262b8
Young999999999 <150986163+Young999999999@users.noreply.github.com>
2024-03-05 23:28:14 +0900

    [Silver I] Title: 계산 로봇, Time: 616 ms, Memory: 173116 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/22342. 계산 로봇/계산 로봇.py
+++ 백준/Silver/22342. 계산 로봇/계산 로봇.py
@@ -1,39 +1,26 @@
+import sys
+input = sys.stdin.readline
+
 n,m = map(int,input().split())
+
 
 matrix = []
 for i in range(n):
-    matrix.append(list(map(int,input())))
+    matrix.append(list(map(int,input().strip())))
 
-
-robot = [[[0,0,0] for i in range(m)] for j in range(n)]
-
-
-# def setStore(x,y):
-#     global result
-#
-#     MAX = 0
-#     for dy in [y-1,y,y+1]:
-#         if -1<dy<n and -1<x-1:
-#             MAX = max(MAX,robot[dy][x-1][2])
-#
-#     result = max(MAX,result)
-#
-#     robot[y][x][1] = MAX
-#     robot[y][x][2] = robot[y][x][1] + matrix[y][x]
-
+output =[[0 for i in range(m+1)] for j in range(n+2)]
 result = 0
 
-for x in range(m):
-    for y in range(n):
-        MAX = 0
-        for dy in [y-1,y,y+1]:
-            if -1<dy<n and -1<x-1:
-                MAX = max(MAX,robot[dy][x-1][2])
 
-        result = max(MAX,result)
-        robot[y][x][1] = MAX
-        robot[y][x][2] = robot[y][x][1] + matrix[y][x]
 
+for x in range(1,m+1):
+    for y in range(1,n+1):
+        MAX = max(output[y - 1][x - 1], output[y][x - 1], output[y + 1][x - 1])
+
+        if result < MAX :
+            result = MAX
+
+        output[y][x] = MAX + matrix[y-1][x-1]
 
 print(result)
 
