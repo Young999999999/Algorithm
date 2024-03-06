27918d9cbe201fcc447ddb79fe79229a1f33d8a5
Young999999999 <150986163+Young999999999@users.noreply.github.com>
2024-03-06 20:14:43 +0900

    [Gold II] Title: 나누기, Time: 120 ms, Memory: 42204 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/21757. 나누기/나누기.py
+++ 백준/Gold/21757. 나누기/나누기.py
@@ -1,12 +1,13 @@
 n = int(input())
 num = list(map(int,input().split()))
 
+
 prefixSum = [0] * (n+1)
 
 for i in range(1,n+1):
     prefixSum[i]= prefixSum[i-1]+ num[i-1]
-prefixSum.pop(0)
 
+prefixSum.pop(0)
 
 dp=[]
 
@@ -19,19 +20,20 @@ elif prefixSum[-1] % 4 == 0 :
     first = prefixSum[-1] // 4
 
     for i in range(n):
-        if prefixSum[i] == first or prefixSum[i] == 2*first or prefixSum[i] == 3*first or prefixSum[i] == 4*first:
+        if  prefixSum[i] == first or prefixSum[i] == 2*first or prefixSum[i] == 3*first or prefixSum[i] == 4*first:
             dp.append(prefixSum[i])
 
     dict = {}
     for i in range(5):
         dict[i*first] = 0
 
+    dict[0] = 1
     for i in dp :
-        if i ==first:
-            dict[i] += 1
+        if i == 4*first:
+            continue
         else:
             dict[i] = dict[i-first] + dict[i]
 
-    print(dict[first*4])
+    print(dict[3*first])
 else:
     print(0)
