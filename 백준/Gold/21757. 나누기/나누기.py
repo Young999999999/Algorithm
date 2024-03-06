2f34e97dbbc5c0e316fb95857fbc97aa5353b80e
Young999999999 <150986163+Young999999999@users.noreply.github.com>
2024-03-06 19:38:43 +0900

    [Gold II] Title: 나누기, Time: 144 ms, Memory: 121728 KB, Score: 7 point -BaekjoonHub

---
+++ 백준/Gold/21757. 나누기/나누기.py
@@ -0,0 +1,36 @@
+n = int(input())
+num = list(map(int,input().split()))
+
+prefixSum = [0] * (n+1)
+
+for i in range(1,n+1):
+    prefixSum[i]= prefixSum[i-1]+ num[i-1]
+prefixSum.pop(0)
+
+
+dp=[]
+
+if prefixSum[-1] == 0:
+    cnt = prefixSum.count(0)
+    print(cnt * (cnt-1) * (cnt-2) // 6)
+
+elif prefixSum[-1] % 4 == 0 :
+    first = prefixSum[-1] // 4
+
+    for i in range(n):
+        if prefixSum[i] == first or prefixSum[i] == 2*first or prefixSum[i] == 3*first or prefixSum[i] == 4*first:
+            dp.append(prefixSum[i])
+
+    dict = {}
+    for i in range(5):
+        dict[i*first] = 0
+
+    for i in dp :
+        if i ==first:
+            dict[i] += 1
+        else:
+            dict[i] = dict[i-first] + dict[i]
+       
+    print(dict[first*4])
+else:
+    print(0)
