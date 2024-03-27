5e8709a9b04b807d4405fde41e65869c2b512093
Young999999999 <150986163+Young999999999@users.noreply.github.com>
2024-03-27 18:32:10 +0900

    [Gold III] Title: 종이접기, Time: 172 ms, Memory: 112560 KB, Score: 63 point -BaekjoonHub

---
+++ 백준/Gold/20187. 종이접기/종이접기.py
@@ -0,0 +1,90 @@
+def RL(matrix): #왼쪽으로 펼치는 그림
+
+    tmp = [[0 for i in range(len(matrix[0]))] for j in range(len(matrix))]
+
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            tmp[i][len(matrix[0])-1-j] = convertX(matrix[i][j])
+
+    return tmp
+
+def DU(matrix):  # 아래로 펼치는 그림
+    tmp = [[0 for i in range(len(matrix[0]))] for i in range(len(matrix))]
+
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            tmp[len(matrix)-1-i][j] = convertY(matrix[i][j])
+
+    return tmp
+
+
+
+def convertX(num):
+    if num == 0 :
+        return 1
+    if num == 2 :
+        return 3
+    if num == 1:
+        return 0
+    if num == 3 :
+        return 2
+
+def convertY(num):
+    if num == 0:
+        return 2
+    if num == 2:
+        return 0
+    if num == 1:
+        return 3
+    if num == 3:
+        return 1
+
+n = int(input())
+opList = input().split()
+matrix = [[int(input())]]
+# print(matrix)
+
+for op in opList:
+
+    if op == 'R':
+        appendix = RL(matrix)
+
+        for i in range(len(matrix)):
+            appendix[i].extend(matrix[i])
+
+        matrix= [i[:] for i in appendix]
+        #
+        # print('R')
+        # print(matrix)
+
+
+    if op == 'L':
+        appendix = RL(matrix)
+
+        for i in range(len(matrix)):
+            matrix[i].extend(appendix[i][:])
+        #
+        # print('L')
+        # for i in matrix:
+        #     print(i)
+
+    if op == 'D':
+        appendix = DU(matrix)
+
+        appendix += matrix
+        matrix = [i[:] for i in appendix]
+        # print('D')
+        # for i in matrix:
+        #     print(i)
+
+    if op == 'U':
+        appendix = DU(matrix)
+        matrix += [i[:] for i in appendix]
+        #
+        # print('U')
+        # for i in matrix:
+        #     print(i)
+
+
+for i in matrix:
+    print(*i)
