229d8d55a7eb44e585ea106f778f24060cb37d3e
Young999999999 <150986163+Young999999999@users.noreply.github.com>
2024-03-30 16:23:48 +0900

    [Silver II] Title: 예산, Time: 176 ms, Memory: 110620 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.py
+++ 백준/Silver/2512. 예산/예산.py
@@ -1,40 +1,24 @@
-import sys
-input =sys.stdin.readline
-
 N = int(input())
+req = list(map(int, input().split()))
+M = int(input())
+start = 1
+end = max(req)
+if sum(req) <= M:
+    print(max(req))
+else:
+    for i in range(1000):
+        mid = (start + end) // 2
+        add_up = 0
 
-money = list(map(int,input().split()))
-limit = int(input())
-
-def BS(low,up):
-    while True:
-        target = (low + up)//2 #정수상한선
-
-        sum = 0 # 예산의 총합
-        for i in money:
-            if i > target: #정수상한선을 넘으면 정수상한선의 값을 넣어줌
-                sum+=target
+        for i in req:
+            if i <= mid:
+                add_up += i
             else:
-                sum+=i
-                if sum >limit: #이미 sum이 limit를 넘는다면 up을 target-1로 바꿔야한다
-                    break #시간초과 방지 break
+                add_up += mid
+        if add_up > M:
+            end = mid - 1
 
-        if sum == limit:
-            print(target)
-            sys.exit()
+        elif add_up < M:
+            start = mid + 1
 
-        if sum > limit : #예산을 초과했다면
-            up=target-1
-            if low>up:
-                print(target-1)
-                sys.exit()
-
-        elif sum < limit:
-            low = target+1
-            if low>up:
-                print(target)
-                sys.exit()
-
-
-up=max(money)
-BS(0,up)
+    print(mid)
