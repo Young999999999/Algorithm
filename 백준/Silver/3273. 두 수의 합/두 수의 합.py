038df1ba7952bed26bea82396b4d99cece17dda7
Young999999999 <150986163+Young999999999@users.noreply.github.com>
2024-05-28 16:26:20 +0900

    [Silver III] Title: 두 수의 합, Time: 504 ms, Memory: 42036 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.py
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.py
@@ -3,21 +3,22 @@ sequence = list(map(int,input().split()))
 X = int(input())
 sequence.sort()
 answer = 0
-
 for i in range(n):
     std = sequence[i]
     l = 0
     r = n-1
     for j in range(20):
         mid = (l+r)//2
-        if std + sequence[mid] > X :
-            r = mid-1
+        if std + sequence[mid] >= X :
+            r = mid
         else:
             if std + sequence[mid] == X:
                 answer += 1
                 break
 
-            l = mid+1
+            l = mid
 
+    if std + sequence[r] == X:
+        answer += 1
 
 print(answer//2)
