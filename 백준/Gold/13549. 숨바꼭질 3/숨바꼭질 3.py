626945af4c5cc957d2dd32f03dd68b1bbe168e2a
Young999999999 <ssas146310@gmail.com>
2024-09-15 20:12:16 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 168 ms, Memory: 35892 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -1,35 +1,64 @@
+import sys
 from collections import deque
-n,k = map(int,input().split())
-q= deque([n])
-cnt=[0] *200001
-visited=[False] * 200001
-vector = [-1,1]
-def bfs():
+input = sys.stdin.readline
+def BFS(node):
+    q=deque()
+    tp=node
+    visited[tp] = True
+    q.append(node)
 
-    while q:
-        now = q.popleft()
+    for i in range(1,10000):
+        tp *=2
+        if tp <= 100000:
+            q.append(tp)
+            visited[tp]=True
+        else :
+            break
 
-        # 순간이동처리
-        for i in range(20):
-            nx = now * (2**i)
-            if nx < 150000 and not visited[nx] :
-                cnt[nx] = cnt[now]
-                visited[nx] = True
-                q.append(nx)
-
-
-        for dx in vector:
-            nx = dx + now
-            if  0<=nx<200000 and not visited[nx] :
-                cnt[nx] = cnt[now] + 1
-                visited[nx] = True
-                q.append(nx)
-
-bfs()
-print(cnt[k])
+    while(q):
+        num = q.popleft()
+        if  num== k :
+            print(cnt_arr[num])
+            break
 
 
+        if num-1 >= 0:
+            num_i =num-1
+            if not visited[num_i] :
+                cnt_arr[num_i] = cnt_arr[num]+1
+                visited[num_i]=True
+                q.append(num_i)
+            tp = num_i
+            for i in range(1, 10000):
+                tp *= 2
+                if tp <= 100000 and not visited[tp]:
+                    q.append(tp)
+                    visited[tp] = True
+                    cnt_arr[tp] = cnt_arr[num_i]
+                if tp > 100000:
+                    break
 
 
+        if num+1 <= 100000:
+            num_ii = num+1
+            if not visited[num_ii] :
+                cnt_arr[num_ii] = cnt_arr[num]+1
+                visited[num_ii]=True
+                q.append(num_ii)
+            tp = num_ii
+            for i in range(1, 10000):
+                tp *=2
+                if tp <= 100000 and not visited[tp]:
+                    q.append(tp)
+                    visited[tp] = True
+                    cnt_arr[tp] = cnt_arr[num_ii]
+                if tp > 100000:
+                    break
 
+
+    return 0
+n,k =map(int,input().split())
+cnt_arr = [0] * 100001
+visited=[False]*100001
+BFS(n)
 
