f28e1e254fc2ba35c1d2be52151dc0a3dd6c453d
Young999999999 <ssas146310@gmail.com>
2024-11-08 16:51:28 +0900

    [Gold IV] Title: LCS 2, Time: 136 ms, Memory: 124364 KB -BaekjoonHub

--- 백준/Gold/9252. LCS 2/LCS 2.py
+++ 백준/Gold/9252. LCS 2/LCS 2.py
@@ -1,24 +1,13 @@
-str1 = input()
-str2 = input()
-
-dp=["" for i in range(len(str2))]
-cost=["" for i in range(len(str2))]
-
-for i in range(len(str1)):
-    for j in range(len(str2)-1,-1,-1):
-        if str1[i] == str2[j]:
-            dp[j] = cost[j] + str1[i]
-
-    MAX = ""
-    for j in range(len(str2)):
-        cost[j] = MAX
-        if len(MAX) < len(dp[j]):
-            MAX = dp[j]
-
-result = ""
-for i in dp:
-    if len(result) < len(i):
-        result = i
-
-print(len(result))
-print(result)
+a,b=input(),input()
+d,c=[""]*len(b),[""]*len(b)
+for i in a:
+ for j in range(len(b)-1,-1,-1):
+  if i==b[j]:d[j]=c[j]+i
+ m=""
+ for j in range(len(b)):
+  c[j]=m
+  if len(m)<len(d[j]):m=d[j]
+r=""
+for i in d:
+ if len(r)<len(i):r=i
+print(len(r),r,sep="\n")
