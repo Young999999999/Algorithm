a9fc50b1c34e7b2dd5e565a9c6c2548d42adeddb
Young999999999 <ssas146310@gmail.com>
2025-05-28 21:04:44 +0900

    [Silver IV] Title: 인사성 밝은 곰곰이, Time: 224 ms, Memory: 39612 KB -BaekjoonHub

--- 백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.js
+++ 백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.js
@@ -2,15 +2,16 @@ fs = require('fs');
 let [n,...lines] = fs.readFileSync(0,'utf8').trim().split('\n');
 let ans = 0;
 
-let dict = {};
+let dict = new Map();
+
 for(let line of lines){
-    if(line === "ENTER") {
-        ans += Object.keys(dict).length; 
-        dict = {};
-    } else{
-        dict[line] = true;
+    if (line === "ENTER") {
+        ans += dict.size;
+        dict.clear();
+    } else {
+        dict.set(line,true);
     }
 }
-ans += Object.keys(dict).length;
 
-console.log(ans)
+ans += dict.size;
+console.log(ans);
